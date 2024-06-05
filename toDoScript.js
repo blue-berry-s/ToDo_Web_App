651835b1715b527ecef08f30861d35e9e2b7970d
blue-berry-s <95601084+blue-berry-s@users.noreply.github.com>
2024-06-05 13:21:40 -0400

    Visuals Completed

--- toDoScript.js
+++ toDoScript.js
@@ -1,6 +1,12 @@
+//Setting up elements from web
 const todo_list = document.querySelector("#todo_container");
 const add_task_button = document.querySelector("#add_task");
 const todo_entry = document.querySelector("input");
+
+//Setting up constants for Class and IDs
+const TASKCLASS = "todo_item"
+const DONECLASS = "done_item"
+
 
 if (localStorage.getItem("user_list") == null){
     let stored_array = {
@@ -15,29 +21,39 @@ else{
 }
 
 
-add_task_button.addEventListener("click", addTasks);
 
-function addTasks(){
 
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "X";
-    deleteButton.setAttribute("class", "delete");
-    deleteButton.addEventListener("click", deleteTasks);
 
-    const newTask = document.createElement("ul");
-    newTask.setAttribute("class", "todo_item");
-    newTask.innerHTML = todo_entry.value;
+add_task_button.addEventListener("click", addTaskBttn);
+todo_entry.addEventListener("keypress", function(event){
+    if (event.key === "Enter"){
+        event.preventDefault();
+        addTaskBttn();
+    }
+});
 
+
+function addTaskBttn(){
     const stored = JSON.parse(localStorage.getItem("user_list"));
 
-    newTask.setAttribute("id", "id_" + stored.tasks.length);
+    addTasksToWeb(todo_entry.value, stored.tasks.length);
 
     stored.tasks.push(todo_entry.value);
     localStorage.setItem("user_list", JSON.stringify(stored)); 
 
-    newTask.append(deleteButton);
-    todo_list.appendChild(newTask);
+    todo_entry.value=null;
 }
+
+function switchClass(event){
+    if (event.target.className == TASKCLASS){
+        event.target.className = DONECLASS
+    }
+    else if (event.target.className == DONECLASS){
+        event.target.className = TASKCLASS
+    }
+    
+}
+
 
 
 function deleteTasks(event){
@@ -57,8 +73,8 @@ function deleteTasks(event){
 }
 
 function updateWebList(){
-    //TODO: REDO --> instead search for childelements to the container (the class will change for crossed out effect)
-    const todo_all = document.querySelectorAll(".todo_item");
+    
+    const todo_all = todo_list.getElementsByTagName("ul");
     for (var i = 0; i < todo_all.length; i++){
         todo_all[i].setAttribute("id", "id_" + i);
     }
@@ -69,26 +85,34 @@ function setUpList(){
     const stored = JSON.parse(localStorage.getItem("user_list"));
 
     for (var i = 0; i< stored.tasks.length; i++){
-        addTasksPrivate(stored.tasks[i], i);
+        addTasksToWeb(stored.tasks[i], i);
     }
 
 }
 
 
-function addTasksPrivate(text, index){
+function addTasksToWeb(text, index){
 
     const deleteButton = document.createElement("button");
     deleteButton.innerHTML = "X";
     deleteButton.setAttribute("class", "delete");
     deleteButton.addEventListener("click", deleteTasks);
 
+    
+
     const newTask = document.createElement("ul");
-    newTask.setAttribute("class", "todo_item");
-    newTask.innerHTML = text;
+    newTask.setAttribute("class", TASKCLASS);
+    const taskText = document.createElement("span");
+    taskText.setAttribute("class", "decorate");
+    taskText.innerHTML = text;
+    newTask.addEventListener("click", switchClass)
 
 
     newTask.setAttribute("id", "id_" + index);
 
+    newTask.append(taskText);
     newTask.append(deleteButton);
     todo_list.appendChild(newTask);
+
+    
 }
