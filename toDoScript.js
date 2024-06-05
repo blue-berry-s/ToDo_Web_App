e046b7d04d1afe1ff90b93d4d7f672f7c7faccf8
blue-berry-s <95601084+blue-berry-s@users.noreply.github.com>
2024-06-05 15:21:01 -0400

    Comments Added

--- toDoScript.js
+++ toDoScript.js
@@ -8,6 +8,7 @@ const TASKCLASS = "todo_item"
 const DONECLASS = "done_item"
 
 
+//Setting up localStorage - Do no overwrite previously set up arrays
 if (localStorage.getItem("user_list") == null){
     let stored_array = {
         tasks: []
@@ -23,8 +24,9 @@ else{
 
 
 
-
+//Add responsivness to buttons on the page
 add_task_button.addEventListener("click", addTaskBttn);
+//Allows user to submit by pressing the Enter Key
 todo_entry.addEventListener("keypress", function(event){
     if (event.key === "Enter"){
         event.preventDefault();
@@ -33,6 +35,8 @@ todo_entry.addEventListener("keypress", function(event){
 });
 
 
+//Adds new tasks to the array in localStorage
+// Calls helper function to add the task visually to the page
 function addTaskBttn(){
     const stored = JSON.parse(localStorage.getItem("user_list"));
 
@@ -44,6 +48,7 @@ function addTaskBttn(){
     todo_entry.value=null;
 }
 
+//Marks tasks as complete by using taking advantage of different CSS styling for "incomplete" and "completed" tasks
 function switchClass(event){
     if (event.target.className == TASKCLASS){
         event.target.className = DONECLASS
@@ -55,23 +60,27 @@ function switchClass(event){
 }
 
 
-
+//Deletes current task based on ID 
 function deleteTasks(event){
 
     const to_delete = event.target.parentElement;
+
+    //deletes the task from the localStorage array
     const index = parseInt(to_delete.id.split('_')[1]);
     const stored = JSON.parse(localStorage.getItem("user_list"));
-
-
     stored.tasks.splice(index,1);
-    
     localStorage.setItem("user_list", JSON.stringify(stored)); 
+
+
+    //deletes the element from the web
     to_delete.remove();
     
+    //make sures HTML element have the proepr Index associated within the ID
     updateWebList()
 
 }
 
+//Updates the tasks id to match their position on the website/ the index in the array
 function updateWebList(){
     
     const todo_all = todo_list.getElementsByTagName("ul");
@@ -90,7 +99,8 @@ function setUpList(){
 
 }
 
-
+//Adds tasks to the website visually by creating HTML elements with the appropriate attributes
+//Each task contains a ID that represents its index in the localStorage array
 function addTasksToWeb(text, index){
 
     const deleteButton = document.createElement("button");
